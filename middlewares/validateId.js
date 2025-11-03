b8c42079783af94377e5be886f3e1f3bfa70e7df
fernandoOS03 <fernando.192023@outlook.es>
2025-11-03 11:37:52 -0500

    se agrega funcion par validar ObjectId

--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -1,10 +1,11 @@
 "Funcion para validar el ObjectId"
-export const validdateObjectId = (res, req, next) => {
+export const validateObjectId = (req, res, next) => {
 
     const { id } = req.params;
+    
     if (!id) {
         return res.status(400).json({ succsess: false, message: "Falta el ID en los parametros de la solicitud" });
-    } else if (id.lenght !== 24) {
+    } else if (id.length !== 24) {
         return res.status(400).json({ succsess: false, message: "El ID proporcionado no tiene un formato válido (debe tener 24 caracteres)." });
     }
 
