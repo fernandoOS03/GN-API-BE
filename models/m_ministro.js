ff99062f2f8f14b1e725612ad8ba32413d6eec7e
fernandoOS03 <fernando.192023@outlook.es>
2025-11-05 16:03:51 -0500

    feat (model) : Imlementacion del Model para la collection Ministro.

---
+++ models/m_ministro.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const MinistroSchema = new mongoose.Schema({
+    nombre_completo : {type:String, require: true},
+    apellido_ministro : String,
+    cargo_ministro : String,
+    foto_ministro:String,
+    telefono_contacto:String
+});
+
+export default mongoose.model('Ministro', MinistroSchema)
