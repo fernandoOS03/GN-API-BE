4f651ef9d03725f96975d7cafbb9195c1d4bd8f2
fernandoOS03 <fernando.192023@outlook.es>
2025-11-07 15:43:53 -0500

    feat : Implementacion de la capa de seguridad

--- controllers/c_auth.js
+++ controllers/c_auth.js
@@ -1,11 +1,25 @@
+import { message } from 'statuses';
 import * as authService from '../services/s_auth.js'
 
-export const Login = async (req, res) =>{
-    const {email, contrasenia} = req.body;
-    try{
-        const {token, rol} = authService.loginUser(email, contrasenia);
+export const Login = async (req, res) => {
+    const { email, contrasenia } = req.body;
 
-    }catch{
+    //Asegura que ambos campos existan
+    if (!email || !contrasenia) {
+        return res.status(400).json({ success: false, message: 'Falta email o contraseña.' });
+    };
 
+    try {
+        const { token, rol } = authService.loginUser(email, contrasenia);
+
+        res.status(200).json({ success: true, message: 'Autenticación exitosa', token, rol });
+
+    } catch (error) {
+        //capturar el error lanzado por el servicio
+        if (error.message === 'Credenciales Invalidas') {
+            return res.status(401).json({ success: false, message: 'Email o contraseña incorrectos.' });
+        };
+        console.error("Error en el login:", error.message);
+        res.status(500).json({ success: false, message: 'Error interno del servidor.' });
     }
 };
---
+++ middlewares/auth.js
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import m_usuario from '../models/m_usuario';
+import { message } from 'statuses';
+
+export const protect = async (req, res, next) => {
+    let token;
+
+    //1. Verifica si el token esta presente en los headers de autorizacion
+    //El formato esperado es: Authorization : Bearer <TOKEN>
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            //2. Obtiene el token  (Eliminando Bearer)
+            token = req.headers.authorization.split(' ')[1];
+
+            //3. Decodifica el token
+            //Verifica la firma, la expiracion  y lo decodifica usando el secreto
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            //4. Adjuntar el Usuario a la peticion
+            //EL 'decoded.usuario.id' viene del payload que se genero en el login
+            //.select('-contrasenia') proviene del hash de la contrasenia que adjunte
+            // a la peticion, aunque ya se tiene 'select : false' en el modelo, esto es doble seguridad 
+            req.user = m_usuario.findById(decoded.usuario.id).select('-contrasenia');
+
+            // 5. continuar el flujo al siguiente controlador/middleware
+            next();
+
+        } catch (error) {
+            console.error("Error en el token de Autorizacion : ", error);
+            // Unauthorzed
+            res.status(401).json({ success: false, message: 'No autorizado, token fallido o expirado. ' });
+        }
+    }
+
+    //Sino hay token en el header
+    if (!token) {
+        res.status(401).json({ success: false, message: 'No autorizado, no se encontró token.' });
+    }
+
+};
